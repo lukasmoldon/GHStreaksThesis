590941ddf6aae429a00bddacd9aa642b2a0ec3fd
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-09-13 20:14:05 +0200

    Added plotStreak.py
    Script for plotting streakdata

---
+++ plotStreak.py
@@ -0,0 +1,31 @@
+# ---------- IMPORT ------------
+import logging
+import matplotlib.pyplot as plt
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source_streakdata = "/home/lmoldon/results/users_streaks.json"
+path_source_groupdata = "/home/lmoldon/results/users_groups.json"
+path_source_usergroupsize = "/home/lmoldon/data/usergroupsize.json"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+path_results_values = "/home/lmoldon/results/streakValues.json"
+path_results_plot = "/home/lmoldon/results/streakPlot.png"
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+# ------------------------------
+
+
+
+# TODO: (Code)
