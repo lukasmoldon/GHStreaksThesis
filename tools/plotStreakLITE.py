96da9cf6ce06b0bb33a7601254a34b2ec9f65905
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-09-15 12:11:35 +0200

    Added plotStreakLite.py
    Uses existing plotdata

---
+++ tools/plotStreakLITE.py
@@ -0,0 +1,54 @@
+# ---------- IMPORT ------------
+import logging
+import json
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source = "/home/lmoldon/results/"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+years = 3 # 3 or 7 years for x axis
+threshold = 100 # threshold of minimum streak length for plot
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+datetimeFormat = "%Y-%m-%d"
+list_of_datetimes = []
+values = []
+path_source = path_source + str(years) + "_years/streakValues" + str(threshold) + ".json"
+# ------------------------------
+
+
+
+logging.info("Accessing plot data ...")
+with open(path_source, "r") as fp:
+    plotdata = json.load(fp)
+
+for entry in plotdata:
+    list_of_datetimes.append(datetime.datetime.strptime(entry, datetimeFormat).date())
+    values.append(plotdata[entry])
+
+logging.info("Done (1/2) ...")
+
+
+logging.info("Creating plot ...")
+dates = matplotlib.dates.date2num(list_of_datetimes)
+matplotlib.pyplot.plot_date(dates, values, '-')
+plt.xlabel("Time")
+plt.ylabel("Avg. streak length")
+
+plt.show()
+logging.info("Done (2/2) ...")
