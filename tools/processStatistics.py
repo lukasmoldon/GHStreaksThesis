9521029f402e06497ad550bae5b41e4c096cfa00
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-09-15 03:18:32 +0200

    Create processStatistics.py
    Get number of users with at least x commits for thresholds in config

---
+++ tools/processStatistics.py
@@ -0,0 +1,61 @@
+# ---------- IMPORT ------------
+import logging
+import ijson
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source = "/home/lmoldon/data/commits_per_user.json"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+# format = "threshold": [over, under]
+data = {
+    "1": [0, 0],
+    "10": [0, 0],
+    "25": [0, 0],
+    "50": [0, 0],
+    "100": [0, 0],
+    "200": [0, 0],
+    "300": [0, 0],
+    "400": [0, 0],
+    "500": [0, 0],
+    "1000": [0, 0],
+    "2000": [0, 0],
+    "5000": [0, 0],
+}
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+# ------------------------------
+
+
+
+logging.info("Starting ...")
+
+cnt=0
+jsonfile = ijson.parse(open(path_source, "r"))
+for prefix, event, value in jsonfile:
+    if event == "number":
+        cnt += 1
+        for key in data:
+            if value >= int(key):
+                data[key][0] += 1
+            else:
+                data[key][1] += 1
+        if cnt % 1000000 == 0:
+            logging.info(str(cnt/1000000) + " million users computed.")
+
+
+logging.info("Users with at least one commit total: " + str(cnt))
+logging.info("Data: ")
+print(data)
+logging.info("Done.")
