592648d635abedad0cd776414a375f4d2e4b1597
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-10-30 19:33:38 +0100

    Update computeSurvivalRatesPerDay.py

--- statistical_tests/streakSurvivalRates/computeSurvivalRatesPerDay.py
+++ statistical_tests/streakSurvivalRates/computeSurvivalRatesPerDay.py
@@ -1,33 +1,56 @@
 # ---------- IMPORT ------------
 import logging
 import datetime
+import json
 # ------------------------------
 
 
 # ---------- INPUT -------------
-path_source = ""
+path_source_streakdata = "/home/lmoldon/data/user_streaks.json"
 # ------------------------------
 
 
 # ---------- OUTPUT ------------
-path_results = ""
+path_results = "/home/lmoldon/data/streakSurvivalRates.json"
 # ------------------------------
 
 
 # ---------- CONFIG ------------
-
+datetimeFormat = "%Y-%m-%d"
+minLength = 15 # minimum streak length for observations
+observed_start = datetime.datetime.strptime("2015-05-19", datetimeFormat).date() # start of observed time, 1 year before the change
+observed_end = datetime.datetime.strptime("2016-05-19", datetimeFormat).date() # end of observed time, day of the change
+#observed_start = datetime.datetime.strptime("2016-05-20", datetimeFormat).date() # start of observed time, one day after the change
+#observed_end = datetime.datetime.strptime("2017-05-20", datetimeFormat).date() # end of observed time, 1 year after the change
 # ------------------------------
 
 
 # ---------- INITIAL -----------
 logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+survivalRates = {} # key = date: value = {"0": #abandoned, "1": #survived}
 # ------------------------------
 
 
 
 log_starttime = datetime.datetime.now()
 
+
+logging.info("Loading data ...")
+with open(path_source_streakdata, "r") as fp:
+    streakdata = json.load(fp)
+logging.info("Done (1/3)")
+
+
 # CODE
+
+logging.info("Done. (2/3)")
+
+
+logging.info("Saving results ...")
+with open(path_results, "w") as fp:
+    json.dump(survivalRates, fp)
+logging.info("Done. (3/3)")
+
 
 log_endtime = datetime.datetime.now()
 log_runtime = (log_endtime - log_starttime)
