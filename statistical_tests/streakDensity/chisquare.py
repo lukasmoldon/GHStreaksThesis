5a752aa49913f4a4430c1ea7b2504a55c9dae790
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-10-30 12:57:17 +0100

    Create chisquare.py
    for testing streak density distribution against null hypothesis / uniform distribution

---
+++ statistical_tests/streakDensity/chisquare.py
@@ -0,0 +1,50 @@
+# ---------- IMPORT ------------
+import logging
+import datetime
+import json
+import scipy
+from scipy.stats import chisquare
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source = "/home/lmoldon/results/contributionsPerBin.json"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+# ------------------------------
+
+
+
+log_starttime = datetime.datetime.now()
+
+
+logging.info("Loading data ...")
+with open(path_source, "r") as fp:
+    bindata = json.load(fp)
+logging.info("Done (1/2)")
+
+
+logging.info("Starting ...")
+
+result = chisquare(bindata)
+
+logging.info("Done. (2/2)")
+
+logging.info("Chi-squared test statistic: " + str(result[0]))
+logging.info("p-value: " + str(result[1]))
+
+
+log_endtime = datetime.datetime.now()
+log_runtime = (log_endtime - log_starttime)
+logging.info("Total runtime: " + str(log_runtime))
