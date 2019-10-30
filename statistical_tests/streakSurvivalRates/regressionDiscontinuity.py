193ce9adce8a71584e9811e8862f8f78f0ee2ba6
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-10-30 13:15:56 +0100

    Create regressionDiscontinuity.py

---
+++ statistical_tests/streakSurvivalRates/regressionDiscontinuity.py
@@ -0,0 +1,35 @@
+# ---------- IMPORT ------------
+import logging
+import datetime
+import rdd # regression discontinuity designs
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source = ""
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+path_results = ""
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+# ------------------------------
+
+
+
+log_starttime = datetime.datetime.now()
+
+# CODE
+
+log_endtime = datetime.datetime.now()
+log_runtime = (log_endtime - log_starttime)
+logging.info("Total runtime: " + str(log_runtime))
