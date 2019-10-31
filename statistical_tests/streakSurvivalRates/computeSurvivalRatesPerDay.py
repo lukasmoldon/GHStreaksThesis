1564554198c49c29ee1748e29b69aff9944a88dd
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-10-31 14:49:04 +0100

    Added bootstrapping to computeSurvivalRatesPerDay.py + bugfix

--- statistical_tests/streakSurvivalRates/computeSurvivalRatesPerDay.py
+++ statistical_tests/streakSurvivalRates/computeSurvivalRatesPerDay.py
@@ -3,6 +3,9 @@ import logging
 import datetime
 from datetime import timedelta, date
 import json
+import numpy as np
+import random
+import scipy.stats
 # ------------------------------
 
 
@@ -19,6 +22,8 @@ path_results = "/home/lmoldon/data/streakSurvivalRates.json"
 # ---------- CONFIG ------------
 datetimeFormat = "%Y-%m-%d"
 minLength = 15 # minimum streak length for observations
+confidenceInterval = 95 # in %
+amountSamples = 1000 # number of bootstrapped samples per day
 observed_start = datetime.datetime.strptime("2015-05-19", datetimeFormat).date() # start of observed time, 1 year before the change
 observed_end = datetime.datetime.strptime("2017-05-20", datetimeFormat).date() # end of observed time, 1 year after the change
 # ------------------------------
@@ -29,6 +34,8 @@ logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt=
 survivalRates = {} # key = date: value = {"0": #abandoned, "1": #survived}
 cnt_streaks_total = 0
 cnt_streaks_observed = 0
+lowerbound = (100 - confidenceInterval) / 2
+upperbound = confidenceInterval + (100 - confidenceInterval) / 2
 # ------------------------------
 
 
@@ -37,6 +44,25 @@ def daterange(observedtime_start, observedtime_end):
         for n in range(int((observedtime_end - observedtime_start).days + 1)):
                 yield observedtime_start + timedelta(n)
 
+def createBootstrapSample(data):
+    i = 0
+    bootstrapSample = []
+    while i < len(data):
+        bootstrapSample.append(random.choice(data))
+        i += 1
+    return bootstrapSample
+
+def getBootstrapSampleAvgs(survived, abandoned, amountSamples):
+    sampleAvgs = []
+    base = [0] * survived + [1] * abandoned
+    random.shuffle(base)
+    i = 0
+    while i < amountSamples:
+        sampleAvgs.append(np.mean(createBootstrapSample(base)))
+        i += 1
+    return sampleAvgs
+
+
 
 log_starttime = datetime.datetime.now()
 
@@ -44,17 +70,19 @@ log_starttime = datetime.datetime.now()
 logging.info("Loading data ...")
 with open(path_source_streakdata, "r") as fp:
     streakdata = json.load(fp)
-logging.info("Done (1/3)")
+logging.info("Done (1/4)")
 
 
-logging.info("Starting ...")
+logging.info("Calculating survival rates...")
 
 # Initial dict for survival rates
 for single_date in daterange(observed_start, observed_end):
         survivalRates[str(single_date.strftime(datetimeFormat))] = { # day X
             "0": 0, # #abandoned (contribution on day X - 1, no contribution on day X)
             "1": 0, # #survived  (contribution on day X - 1, contribution on day X)
-            "r": 0  # survival rate
+            "r": 0, # survival rate (avg)
+            "a": 0, # 2.5th% of x bootstrapped avg values (lower bound of 95% confidence interval)
+            "b": 0  # 97.5th% of x bootstrapped avg values (upper bound of 95% confidence interval)
         }
 
 for userid in streakdata: # for each user
@@ -87,17 +115,29 @@ for date in survivalRates:
         survivalRates[date]["r"] = survivalRates[date]["1"] / (survivalRates[date]["1"] + survivalRates[date]["0"])
     elif survivalRates[date]["0"] == 0: # if both are zero, the survival rate is 1
         survivalRates[date]["r"] = 1
-    elif survivalRates[date]["1"] == 0: # if abandodend number is not zero, but survived number is zero, survival rate is 0
+    elif survivalRates[date]["1"] == 0: # if abandoned number is not zero, but survived number is zero, survival rate is 0
         survivalRates[date]["r"] = 0
     
+logging.info("Done. (2/4)")
 
-logging.info("Done. (2/3)")
+
+logging.info("Calculating confidence intervals ...")
+i = 0
+for date in survivalRates:
+    sampleAvgs = getBootstrapSampleAvgs(survivalRates[date]["0"], survivalRates[date]["1"], amountSamples)
+    survivalRates[date]["a"] = np.percentile(sampleAvgs, lowerbound)
+    survivalRates[date]["b"] = np.percentile(sampleAvgs, upperbound)
+    i += 1
+    if i % 100 == 0:
+        logging.info(str(i) + " days computed.") 
+
+logging.info("Done. (3/4)")
 
 
 logging.info("Saving results ...")
 with open(path_results, "w") as fp:
     json.dump(survivalRates, fp)
-logging.info("Done. (3/3)")
+logging.info("Done. (4/4)")
 
 
 logging.info("Total streaks: " + str(cnt_streaks_total))
