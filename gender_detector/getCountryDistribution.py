ec2e37fed14bd64c80f81030d44f74b7a98207bd
Lukas <49486044+TheLukester@users.noreply.github.com>
2019-11-12 13:45:15 +0100

    Create getCountryDistribution.py

---
+++ gender_detector/getCountryDistribution.py
@@ -0,0 +1,60 @@
+# ---------- IMPORT ------------
+import logging
+import datetime
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source = "/home/lmoldon/data/users_gender.json"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+path_results = "/home/lmoldon/results/countryDistribution.json"
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+userdata = {}
+countries = {}
+# ------------------------------
+
+
+
+log_starttime = datetime.datetime.now()
+
+
+logging.info("Loading data:")
+
+logging.info("Loading userdata ...")
+with open(path_source, "r") as fp:
+    userdata = json.load(fp)
+
+logging.info("Done.")
+
+
+for userid in userdata:
+    cur_country = userdata[userid]["country"]
+    if cur_country in countries:
+        countries[cur_country] += 1
+    else:
+        countries[cur_country] = 1
+
+
+logging.info("Saving data ...")
+
+with open(path_results, "w") as fp:
+    json.dump(countries, fp)
+
+logging.info("Done.")
+
+
+log_endtime = datetime.datetime.now()
+log_runtime = (log_endtime - log_starttime)
+logging.info("Total runtime: " + str(log_runtime))
