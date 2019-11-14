5298d7057b720dec2ff53a283860ba56a3b99a6f
Lukas Moldon <lukas.moldon@rwth-aachen.de>
2019-11-14 22:11:11 +0100

    Update logisticRegression.py

--- statistical_tests/streakSurvivalRates/logisticRegression.py
+++ statistical_tests/streakSurvivalRates/logisticRegression.py
@@ -18,7 +18,8 @@ path_source = "/home/lmoldon/results/regressionData.csv"
 
 
 # ---------- CONFIG ------------
-
+showCorrMatrix = False
+showCountUniqueValues = False
 # ------------------------------
 
 
@@ -34,6 +35,26 @@ log_starttime = datetime.datetime.now()
 rawData = pd.read_csv(path_source)
 regressionData = rawData.drop(["userday", "sunday"], axis=1)
 
+if showCorrMatrix:
+    logging.info("Correleation matrix:")
+    print(regressionData.corr())
+    print()
+
+if showCountUniqueValues:
+    logging.info("Total unique values distibution:")
+    print(rawData.groupby("contribution_that_day")["userday"].nunique())
+    print(rawData.groupby("after_change")["userday"].nunique())
+    print(rawData.groupby("current_streak")["userday"].nunique())
+    print(rawData.groupby("monday")["userday"].nunique())
+    print(rawData.groupby("tuesday")["userday"].nunique())
+    print(rawData.groupby("wednesday")["userday"].nunique())
+    print(rawData.groupby("thursday")["userday"].nunique())
+    print(rawData.groupby("friday")["userday"].nunique())
+    print(rawData.groupby("saturday")["userday"].nunique())
+    print(rawData.groupby("sunday")["userday"].nunique())
+    print()
+
+logging.info("Logistic regression results: ")
 logit = smf.logit("contribution_that_day ~ after_change +np.log(current_streak+.5) +after_change:np.log(current_streak+.5) +monday+tuesday+wednesday+thursday+friday+saturday", regressionData)
 result = logit.fit()
 result.summary()
