2b4d360f57dfead09621c3888d8bf481fc360034
Lukas Moldon <49486044+lukasmoldon@users.noreply.github.com>
2020-01-16 16:42:01 +0100

    Added plotting for SurvivalAroundLastRecord with errorbars

---
+++ plot/survivalAroundLastRecord/plotSurvivalAroundLastRecordErrorBars.py
@@ -0,0 +1,72 @@
+# ---------- IMPORT ------------
+import logging
+import json
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+# ------------------------------
+
+
+# ---------- INPUT -------------
+
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+x = []
+values = []
+# ------------------------------
+
+
+lower_error = []
+upper_error = []
+with open("C:/Users/Lukas/Desktop/survivalAroundLastRecordMIN25DIST30BEFORE_ERRORB.json", "r") as fp:
+    plotdata = json.load(fp)
+
+del plotdata["__TOTAL__"]
+
+for entry in plotdata:
+    values.append(plotdata[entry]["r"])
+    lower_error.append(plotdata[entry]["a"])
+    upper_error.append(plotdata[entry]["b"])
+    x.append(entry)
+
+asymmetric_error = [lower_error, upper_error]
+
+matplotlib.pyplot.errorbar(x, values, yerr=asymmetric_error, fmt='o', label="Before")
+plt.xlabel("Streak days arround the last record")
+plt.ylabel("Survival rate of new streak within 30 days after last record")
+
+
+values = []
+lower_error = []
+upper_error = []
+with open("C:/Users/Lukas/Desktop/survivalAroundLastRecordMIN25DIST30AFTER_ERRORB.json", "r") as fp:
+    plotdata = json.load(fp)
+
+del plotdata["__TOTAL__"]
+
+for entry in plotdata:
+    values.append(plotdata[entry]["r"])
+    lower_error.append(plotdata[entry]["a"])
+    upper_error.append(plotdata[entry]["b"])
+
+asymmetric_error = [lower_error, upper_error]
+
+matplotlib.pyplot.errorbar(x, values, yerr=asymmetric_error, fmt='o', label="After")
+
+
+plt.axvline(x="0", color='r', label="Last maximum streak")
+plt.legend()
+plt.show()
