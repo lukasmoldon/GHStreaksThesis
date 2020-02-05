8c4ca5d7b2c9bcf5e0b97b7f46bb748b53564368
Lukas Moldon <49486044+lukasmoldon@users.noreply.github.com>
2020-02-05 15:47:12 +0100

    Create repairCommitComments.py

---
+++ repairCSV/repairCommitComments.py
@@ -0,0 +1,81 @@
+# ---------- IMPORT ------------
+import logging
+import datetime
+import json
+import csv
+import re
+import pandas as pd
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source = "C:/Users/Lukas/Desktop/commit_comments.csv"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+path_results = "C:/Users/Lukas/Desktop/test.csv"
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+datetimeFormat = "%Y-%m-%d"
+# ------------------------------
+
+
+logging.info("Starting ...")
+with open(path_source, "r", encoding='utf-8') as infile:
+    reader = csv.reader(infile)
+    globalindex = 0
+    data = {}
+    withinBroken = False # flag == True, if we have not found the end of a real row in current row
+    currow = ""
+    for row in reader: # be aware that multiple rows can correspond one single real row we want to see
+        if row != []:
+            if not withinBroken: # new real row starts here
+                if len(row) == 8 and len(re.findall("^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$", str(row[-1]))) == 1: # this row is complete
+                    row[3] = row[3].replace("\n", " ").replace("\\", "").replace(",", "").replace("\"", "").replace("\'", "").replace("`", "")
+                    data[globalindex] = row
+                    globalindex += 1
+                    if (globalindex % 1000000 == 0):
+                        logging.info(str(globalindex/1000000) + " million comments computed")
+                else: # end of this new row is missing
+                    i = 3
+                    tmp = ""
+                    while i < len(row):
+                        tmp += str(row[i]).replace("\n", " ").replace("\\", "").replace(",", "").replace("\"", "").replace("\'", "").replace("`", "")
+                        i += 1
+                    currow = [row[0]] + [row[1]] + [row[2]] + [tmp]
+                    withinBroken = True
+            else: # we have an open real row to be closed (find end)
+                if len(re.findall("^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$", str(row[-1]))) == 1: # we found the end
+                    i = 0
+                    tmp = ""
+                    while i < (len(row) - 4):
+                        tmp += str(row[i]).replace("\n", " ").replace("\\", "").replace(",", "").replace("\"", "").replace("\'", "").replace("`", "")
+                        i += 1
+                    currow = [currow[0]] + [currow[1]] + [currow[2]] + [currow[3] + tmp] + [row[-4]] + [row[-3]] + [row[-2]] + [row[-1]]
+                    withinBroken = False
+                    currow[3] = currow[3].replace("\n", " ").replace("\\", "").replace(",", "").replace("\"", "").replace("\'", "").replace("`", "")
+                    data[globalindex] = currow
+                    globalindex += 1
+                    if (globalindex % 1000000 == 0):
+                        logging.info(str(globalindex/1000000) + " million comments computed")
+                else: # no end was found - we are in the middle part of a row
+                    i = 0
+                    tmp = ""
+                    while i < len(row):
+                        tmp += str(row[i]).replace("\n", " ").replace("\\", "").replace(",", "").replace("\"", "").replace("\'", "").replace("`", "")
+                        i += 1
+                    currow[3] += tmp   
+
+logging.info("Saving resulting data...")
+data = pd.DataFrame.from_dict(data, "index")
+data.to_csv(path_results, encoding='utf-8', index=False, header=None)
+logging.info("Done!")
