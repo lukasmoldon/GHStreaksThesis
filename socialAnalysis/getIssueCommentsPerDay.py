adc39cb9543071e3d18d3d8b6dcf5e8c4ffa251e
Lukas Moldon <49486044+lukasmoldon@users.noreply.github.com>
2020-02-10 16:09:20 +0100

    Added country restriction for comment analysis

--- socialAnalysis/getIssueCommentsPerDay.py
+++ socialAnalysis/getIssueCommentsPerDay.py
@@ -13,6 +13,7 @@ import time
 # ---------- INPUT -------------
 path_source = "/home/johannes/data/github/mysql-2019-06-01/issue_comments.csv"
 path_source_userids = "/home/lmoldon/data/users_reduced_IDs.json"
+path_source_gender = "/home/lmoldon/data/users_gender.json"
 # ------------------------------
 
 
@@ -30,6 +31,8 @@ chunksize = 1000000
 logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
 datetimeFormat = "%Y-%m-%d %H:%M:%S"
 data = {}
+country_restricted = True # True == only count users from country_restrictions
+country_restrictions = ["United Kingdom"]
 # ------------------------------
 
 
@@ -40,28 +43,35 @@ logging.info("Loading data ...")
 with open(path_source_userids, "r") as fp:
     userids = json.load(fp)
 
+with open(path_source_gender, "r") as fp:
+    genderdata = json.load(fp)
+logging.info("Done (1/3)")
+
+
+logging.info("Starting ...")
 cnt = 0
 for chunk in pd.read_csv(path_source, chunksize=chunksize, header=None, delimiter=",", encoding='utf-8'):
     for row in list(chunk.values):
         if str(row[1]) in userids:
-            try:
-                timestamp = datetime.datetime.strptime(str(row[3]), datetimeFormat).date()
-                cnt += 1
-                if cnt % 1000000 == 0: logging.info(str(cnt/1000000) + " million comments computed")
-                if str(timestamp) in data:
-                    data[str(timestamp)] += 1
-                else:
-                    data[str(timestamp)] = 1
-            except:
-                logging.warning("Could not read following line:")
-                logging.warning(str(row))
-logging.info("Done (1/2)")
+            if not country_restricted or str(genderdata[str(row[1])]["country"]) in country_restrictions:
+                try:
+                    timestamp = datetime.datetime.strptime(str(row[3]), datetimeFormat).date()
+                    cnt += 1
+                    if cnt % 1000000 == 0: logging.info(str(cnt/1000000) + " million comments computed")
+                    if str(timestamp) in data:
+                        data[str(timestamp)] += 1
+                    else:
+                        data[str(timestamp)] = 1
+                except:
+                    logging.warning("Could not read following line:")
+                    logging.warning(str(row))
+logging.info("Done (2/3)")
 
 
 logging.info("Saving ...")
 with open(path_results, "w") as fp:
     json.dump(data, fp)
-logging.info("Done (2/2)")
+logging.info("Done (3/3)")
 
 
 log_endtime = datetime.datetime.now()
