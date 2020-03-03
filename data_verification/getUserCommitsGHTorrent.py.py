bbb21d65b1fd1ec2ebe35ae944e92b3c73eec0e9
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-03-03 17:02:39 +0100

    Rework data verification

--- data_verification/getUserCommitsGHTorrent.py.py
+++ data_verification/getUserCommitsGHTorrent.py.py
@@ -3,28 +3,31 @@ import pandas as pd
 import logging
 import datetime
 import json
+import sys
 # ------------------------------
 
 
 # ---------- INPUT -------------
-path_source_issues = "/home/johannes/data/github/mysql-2019-06-01/issues.csv"
+path_source_commits = "/home/johannes/data/github/mysql-2019-06-01/commits.csv"
+
+userID = sys.argv[1]
 # ------------------------------
 
 
 # ---------- OUTPUT ------------
-path_results = "/home/lmoldon/results/verification_issues.json"
+path_results = "..."
 # ------------------------------
 
 
 # ---------- CONFIG ------------
 chunksize = 1000000
-userID = "10891816"
 # ------------------------------
 
 
 # ---------- INITIAL -----------
 logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
-issuedata = {} # store issuedata (key = issueid, value = issuedata)
+data = {} 
+path_results = "/home/lmoldon/results/verification/commits_" + userID + ".json"
 # ------------------------------
 
 
@@ -32,24 +35,22 @@ issuedata = {} # store issuedata (key = issueid, value = issuedata)
 log_starttime = datetime.datetime.now()
 
 
-
-# store issuedata for all issues with issuer_ids in users_reduced_IDs.json
-logging.info("Accessing issuedata ...")
+logging.info("Starting ...")
 cnt = 0
-for chunk in pd.read_csv(path_source_issues, chunksize=chunksize, header=None, delimiter=",", usecols=[0,1,2,6], names=["id","repo_id","reporter_id","created_at"]):
+for chunk in pd.read_csv(path_source_commits, chunksize=chunksize, header=None, delimiter=",", usecols=[0,3,4,5], names=["id","committer_id","project_id","created_at"]):
     for row in list(chunk.values):
-        if str(row[2]) == "10891816":
-            if str(row[3]) in issuedata:
-                issuedata[str(row[3])] += 1
+        if str(row[1]) == userID:
+            if str(row[3]) in data:
+                data[str(row[3])] += 1
             else:
-                issuedata[str(row[3])] = 1
+                data[str(row[3])] = 1
     cnt += 1
     logging.info("Chunk counter = " + str(cnt))
 
 
-logging.info("Storing data ... ")
+logging.info("Storing data ...")
 with open(path_results, "w") as fp:
-    json.dump(issuedata, fp)
+    json.dump(data, fp)
 
 logging.info("Done.")
 
