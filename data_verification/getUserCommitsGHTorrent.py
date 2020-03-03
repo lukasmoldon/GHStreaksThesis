84ab660ab11437d8a10ecef74efc8e2cac7a2a22
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-03-03 16:57:18 +0100

    Rework single requests for GHTorrent sided data verification

--- data_verification/getUserCommitsGHTorrent.py
+++ data_verification/getUserCommitsGHTorrent.py
@@ -3,16 +3,19 @@ import pandas as pd
 import logging
 import datetime
 import json
+import sys
 # ------------------------------
 
 
 # ---------- INPUT -------------
 path_source_commits = "/home/johannes/data/github/mysql-2019-06-01/commits.csv"
+
+userID = sys.argv[1]
 # ------------------------------
 
 
 # ---------- OUTPUT ------------
-path_results = "/home/lmoldon/scripts/dates.json"
+path_results = "..."
 # ------------------------------
 
 
@@ -23,7 +26,8 @@ chunksize = 1000000
 
 # ---------- INITIAL -----------
 logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
-data = {} # dict for saving IDs as keys and amount of commits as value
+data = {} 
+path_results = "/home/lmoldon/results/verification/commits_" + userID + ".json"
 # ------------------------------
 
 
@@ -35,7 +39,7 @@ logging.info("Starting ...")
 cnt = 0
 for chunk in pd.read_csv(path_source_commits, chunksize=chunksize, header=None, delimiter=",", usecols=[0,3,4,5], names=["id","committer_id","project_id","created_at"]):
     for row in list(chunk.values):
-        if str(row[1]) == "10891816":
+        if str(row[1]) == userID:
             if str(row[3]) in data:
                 data[str(row[3])] += 1
             else:
---
+++ data_verification/getUserContributionsGHAPI.py
@@ -0,0 +1,273 @@
+# ---------- IMPORT ------------
+import requests
+import requests_html
+import datetime
+from datetime import timedelta
+import json
+import time
+import logging
+import sys
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source_userdata = "/home/lmoldon/data/users_reduced.json"
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+path_results = "..."
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+username = "XXXXXXXXXXXXXXXXXXXXX"
+token = "XXXXXXXXXXXXXXXXXXXXX"
+email = "XXXXXXXXXXXXXXXXXXXXX"
+useragent = "Research for bachelor thesis on GitHub streaks" 
+
+chunksize = 1000000
+
+debugmode = False # Enable debug messages in console
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+userdata = {} # from reduced_users
+userid = sys.argv[1]
+path_results = "/home/lmoldon/results/verification/contributions_" + userid + ".json"
+
+if debugmode:
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.DEBUG)
+else:
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+
+useragent = useragent + "  Contact: " + email
+session = requests_html.HTMLSession()
+
+link_userinfo = "https://api.github.com/users/"
+
+nextreq = datetime.datetime.now()
+
+repo_collection = set()
+
+queryresult = {}
+# ------------------------------
+
+
+
+def sleep_epoch(epochtime):
+    start = datetime.datetime.fromtimestamp(int(epochtime) + 10)
+    now = datetime.datetime.now().replace(microsecond=0)
+    logging.info("Waiting for API counter reset in " + str(start - now))
+    time.sleep((start - now).seconds)
+    logging.info("Stopped waiting!")
+
+
+
+log_starttime = datetime.datetime.now()
+
+
+
+logging.info("Loading userdata ...")
+
+with open(path_source_userdata, "r") as fp:
+    userdata = json.load(fp)
+
+cur_username = userdata[userid]["name"]
+link_userrepo = "https://api.github.com/users/" + cur_username + "/repos?per_page=100&page="
+
+logging.info("Done.")
+
+
+
+logging.info("Connecting with api.github.com:")
+
+cnt_triesGH = 0
+done = False
+while(not done):
+    try:
+        answer = session.get(link_userinfo + "lukasmoldon", auth=(username, token))
+
+        status = answer.headers["Status"]
+        if status != "200 OK":
+            if int(answer.headers["X-RateLimit-Remaining"]) == 0:
+                logging.info("API counter at 0!")
+                sleep_epoch(answer.headers["X-RateLimit-Reset"])
+                answer = session.get(link_userinfo + "lukasmoldon", auth=(username, token))
+
+        if status == "200 OK":
+            done = True
+            logging.info("Requests remaining for GitHub API: " + str(int(answer.headers["X-RateLimit-Remaining"])))
+        else:
+            logging.error("Received unexpected answer while connecting with api.github.com:")
+            print(answer.headers)
+            print(answer.text)
+            time.sleep(0.72)
+            cnt_triesGH += 1
+            if cnt_triesGH > 50:
+                logging.fatal("Too many failed GET requests without 200 OK:")
+                print(answer.headers)
+                print(answer.text)
+                sys.exit()
+    except:
+        logging.warning("Could not send GET request to api.github.com")
+        time.sleep(0.72)
+        cnt_triesGH += 1
+        if cnt_triesGH > 50:
+            logging.fatal("Too many failed GET requests")
+            sys.exit()
+
+logging.info("Done.")
+
+
+
+logging.info("Starting ...")
+
+
+answer = ["_"]
+repo_page = 0
+while(len(answer) > 0):
+    repo_page += 1
+    while(not done):
+        try:
+            answer = session.get(link_userrepo + str(repo_page), auth=(username, token))
+
+            status = answer.headers["Status"]
+            if status == "404 Not Found" or status == "403 Forbidden":
+                done = True
+                skip = True
+            elif status != "200 OK":
+                if int(answer.headers["X-RateLimit-Remaining"]) == 0:
+                    logging.info("API counter at 0!")
+                    sleep_epoch(answer.headers["X-RateLimit-Reset"])
+                    answer = session.get(link_userrepo + str(repo_page), auth=(username, token))
+
+            if not skip:
+                if status == "200 OK":
+                    done = True
+                else:
+                    logging.error("Received unexpected answer while connecting with api.github.com:")
+                    print(link_userinfo + cur_username)
+                    print(answer.headers)
+                    print(answer.text)
+                    time.sleep(0.72)
+                    cnt_triesGH += 1
+                    if cnt_triesGH > 50:
+                        logging.fatal("Too many failed GET requests without 200 OK:")
+                        print(link_userinfo + cur_username)
+                        print(answer.headers)
+                        print(answer.text)
+                        sys.exit()
+        except:
+            logging.warning("Could not send GET request to api.github.com")
+            time.sleep(0.72)
+            cnt_triesGH += 1
+            if cnt_triesGH > 50:
+                logging.critical("Too many failed GET requests. Waiting 60 min ...")
+                time.sleep(3600)
+            elif cnt_triesGH > 70:
+                logging.fatal("Too many failed GET requests")
+                sys.exit()
+
+    try:
+        if int(answer.headers["X-RateLimit-Remaining"]) % 1000 == 0:
+            logging.info("GitHub requests remaining: " + str(answer.headers["X-RateLimit-Remaining"]))
+    except:
+        logging.warning("Unexpected Error occurred while accessing remaining request number at api.github.com:")
+        print(answer.headers)
+        print(answer.text)
+            
+    if not skip:
+        try:
+            for repo in answer.json():
+                repo_collection.add(repo["url"])
+                answer = answer.json()
+        except:
+            logging.debug("Could not compute repo entries for: " + str(cur_username))
+    else:
+        logging.debug("Username not found: " + str(cur_username))
+
+
+for link_repo in repo_collection:
+    commit_page = 0
+    link_repo = link_repo + "/commits?per_page=100&page="
+    answer = ["_"]
+
+    while(len(answer) > 0):
+        commit_page += 1
+        while(not done):
+            try:
+                answer = session.get(link_repo + str(commit_page), auth=(username, token))
+
+                status = answer.headers["Status"]
+                if status == "404 Not Found" or status == "403 Forbidden":
+                    done = True
+                    skip = True
+                elif status != "200 OK":
+                    if int(answer.headers["X-RateLimit-Remaining"]) == 0:
+                        logging.info("API counter at 0!")
+                        sleep_epoch(answer.headers["X-RateLimit-Reset"])
+                        answer = session.get(link_repo + str(commit_page), auth=(username, token))
+
+                if not skip:
+                    if status == "200 OK":
+                        done = True
+                    else:
+                        logging.error("Received unexpected answer while connecting with api.github.com:")
+                        print(link_userinfo + cur_username)
+                        print(answer.headers)
+                        print(answer.text)
+                        time.sleep(0.72)
+                        cnt_triesGH += 1
+                        if cnt_triesGH > 50:
+                            logging.fatal("Too many failed GET requests without 200 OK:")
+                            print(link_userinfo + cur_username)
+                            print(answer.headers)
+                            print(answer.text)
+                            sys.exit()
+            except:
+                logging.warning("Could not send GET request to api.github.com")
+                time.sleep(0.72)
+                cnt_triesGH += 1
+                if cnt_triesGH > 50:
+                    logging.critical("Too many failed GET requests. Waiting 60 min ...")
+                    time.sleep(3600)
+                elif cnt_triesGH > 70:
+                    logging.fatal("Too many failed GET requests")
+                    sys.exit()
+
+        try:
+            if int(answer.headers["X-RateLimit-Remaining"]) % 1000 == 0:
+                logging.info("GitHub requests remaining: " + str(answer.headers["X-RateLimit-Remaining"]))
+        except:
+            logging.warning("Unexpected Error occurred while accessing remaining request number at api.github.com:")
+            print(answer.headers)
+            print(answer.text)
+                
+        if not skip:
+            try:
+                for contribution in answer.json():
+                    # make sure that commit is performed by corresponding user
+                    # (repos can be share by several users)
+                    if contribution["author"]["login"] == cur_username:
+                        queryresult[contribution["commit"]["date"]] = 0
+            except:
+                logging.debug("Could not compute repo entries for: " + str(cur_username))
+        else:
+            logging.debug("Username not found: " + str(cur_username))
+
+
+logging.info("Storing data ...")
+with open(path_results, "w") as fp:
+    json.dump(queryresult, fp)
+
+
+
+logging.info("Done.")
+
+
+log_endtime = datetime.datetime.now()
+log_runtime = (log_endtime - log_starttime)
+logging.info("Total runtime: " + str(log_runtime))
