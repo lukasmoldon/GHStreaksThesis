f256524d2dc800e9a8e95e1485d2ca03d0ca6110
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-03-04 11:15:28 +0100

    Rework for usergorupsize (added gender)

--- streak_computation/getUsergroupsize.py
+++ streak_computation/getUsergroupsize.py
@@ -1,19 +1,21 @@
 # ---------- IMPORT ------------
 import logging
 import json
-import ijson
 import datetime
 from datetime import timedelta, date
 # ------------------------------
 
 
 # ---------- INPUT -------------
-path_source = "/home/lmoldon/data/users_reduced.json"
+path_source_userdata = "/home/lmoldon/data/users_reduced.json"
+path_source_genderdata = "/home/lmoldon/data/users_gender.json"
 # ------------------------------
 
 
 # ---------- OUTPUT ------------
-path_results = "/home/lmoldon/data/usergroupsize.json"
+path_results = "/home/lmoldon/data/usergroupsizeALL.json"
+path_results_male = "/home/lmoldon/data/usergroupsizeMALE.json"
+path_results_female = "/home/lmoldon/data/usergroupsizeFEMALE.json"
 # ------------------------------
 
 
@@ -25,70 +27,83 @@ path_results = "/home/lmoldon/data/usergroupsize.json"
 logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
 datetimeFormat = "%Y-%m-%d %H:%M:%S"
 groupsize = {} # store size of observed usergroup "users_reduced.json" for each day (key = date, value = size of usergroup on that day)
-entry_dates = {} # temporary store entry dates of observed usergroup "users_reduced.json" for each day (key = date, value = number of users joined GitHub)
-start_date = date(2016, 5, 19)
-end_date = date(2016, 5, 19)
+groupsize_female = {}
+groupsize_male = {}
+start_date = date(2011, 1, 1)
+end_date = date(2019, 4, 1)
 
 def daterange(observedtime_start, observedtime_end):
-        for n in range(int((observedtime_end - observedtime_start).days)):
-                yield observedtime_start + timedelta(n)
+    for n in range(int((observedtime_end - observedtime_start).days + 1)):
+        yield observedtime_start + timedelta(n)
 # ------------------------------
 
 
 
 log_starttime = datetime.datetime.now()
 
-# Get GitHub entry dates of all observed users and save them per date
 logging.info("Accessing userdata ...")
+with open(path_source_genderdata, "r") as fp:
+    genderdata = json.load(fp)
+with open(path_source_userdata, "r") as fp:
+    userdata = json.load(fp)
+logging.info("Done (1/3)")
+
+
+logging.info("Starting ...")
+
+for day in daterange(start_date, end_date):
+    groupsize[str(day)] = 0
+    groupsize_male[str(day)] = 0
+    groupsize_female[str(day)] = 0
+
 cnt = 0
-jsonfile = ijson.parse(open(path_source, "r"))
-for prefix, event, value in jsonfile:
-    if ".created_at" in prefix: # prefix = userid.created_at, value = date
-        cnt += 1
-        if cnt % 100000 == 0:
-            logging.info(str(cnt) + " users computed.")
+for userid in userdata:
 
-        day = datetime.datetime.strptime(str(value), datetimeFormat).date()
-        if day > end_date:
-            end_date = day
-        if day < start_date:
-            start_date = day
-
-        day = str(day)
-        if day in entry_dates:
-            entry_dates[day] += 1
-        else:
-            entry_dates[day] = 1
-        
-logging.info("Done. (1/2)")
-
-
-# Calculate size of usergroup per day in interval [start_date, end_date]
-logging.info("Calculating size of usergroup per day...")
-cnt = 0
-for single_date in daterange(start_date, end_date):
-    groupsize[str(single_date)] = 0
-
-for single_date in daterange(start_date, end_date):
-    if str(single_date) in entry_dates:
-        for following_date in daterange(single_date, end_date):
-            groupsize[str(following_date)] += entry_dates[str(single_date)]
-    else:
-        logging.warning("No users of observed group joined on " + str(single_date))
     cnt += 1
-    if cnt % 100 == 0:
-        logging.info("Days computed: " + str(cnt))
+    if (cnt%10000)==0: logging.info(str(cnt/1000) + "k users computed")
     
+    created = datetime.datetime.strptime(str(userdata[userid]["created_at"]), "%Y-%m-%d %H:%M:%S").date()
+    if created >= start_date and created <= end_date:
+        for day in daterange(created, end_date):
+            groupsize[str(day)] += 1
+    elif created < start_date:
+        for day in daterange(start_date, end_date):
+            groupsize[str(day)] += 1
+
+    if userid in genderdata:
+
+        if genderdata[userid]["gender"] == "male":
+            if created >= start_date and created <= end_date:
+                for day in daterange(created, end_date):
+                    groupsize_male[str(day)] += 1
+            elif created < start_date:
+                for day in daterange(start_date, end_date):
+                    groupsize_male[str(day)] += 1
+
+        elif genderdata[userid]["gender"] == "female":
+            if created >= start_date and created <= end_date:
+                for day in daterange(created, end_date):
+                    groupsize_female[str(day)] += 1
+            elif created < start_date:
+                for day in daterange(start_date, end_date):
+                    groupsize_female[str(day)] += 1
+
+        
+logging.info("Done. (2/3)")
 
 
 logging.info("Storing data ...")
 with open(path_results, "w") as fp:
     json.dump(groupsize, fp)
+with open(path_results_male, "w") as fp:
+    json.dump(groupsize_male, fp)
+with open(path_results_female, "w") as fp:
+    json.dump(groupsize_female, fp)
+logging.info("Done. (3/3)")
 
-logging.info("Start date: " + str(start_date))
-logging.info("End date: " + str(end_date))
-logging.info("Total amount of days in range: " + str(cnt))
-logging.info("Done. (2/2)")
+logging.info("Total users: " + str(groupsize[str(end_date)]))
+logging.info("Total MALE users: " + str(groupsize_male[str(end_date)]))
+logging.info("Total FEMALE users: " + str(groupsize_female[str(end_date)]))
 
 log_endtime = datetime.datetime.now()
 log_runtime = (log_endtime - log_starttime)
