9feb8f5c58568597f219f75dd44b8618866c64a0
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-03-04 18:44:43 +0100

    Create computeShareOfUsersStreaking.py

---
+++ streak_computation/computeShareOfUsersStreaking.py
@@ -0,0 +1,190 @@
+# ---------- IMPORT ------------
+import logging
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+import json
+import ijson
+from datetime import timedelta, date
+# ------------------------------
+
+
+# ---------- INPUT -------------
+path_source_userdata = "/home/lmoldon/data/users_reduced.json"
+path_source_streakdata = "/home/lmoldon/data/user_streaks.json"
+path_source_usergroupsize = "/home/lmoldon/data/usergroupsizeALL.json"
+path_source_genderdata = "/home/lmoldon/data/users_gender.json"
+path_user_restriction = ""
+# ------------------------------
+
+
+# ---------- OUTPUT ------------
+# RENAME IF RESTRICTION IS ACTIVE TO AVOID OVERRIDE
+path_results = "/home/lmoldon/results/streakShareValues.json"
+# ------------------------------
+
+
+# ---------- CONFIG ------------
+thresholds = [50, 150, 350]  # minimum streak length to get plotted
+
+# if != "" only compute data for userIDs in user_restriction file (path)
+path_user_restriction = ""
+
+# "male" or "female" or "",  if == "" all users
+gender_restriction = ""
+
+# e.g. ["USA", "China"] or ["Germany"]
+country_restriction = []
+
+observedtime_start = date(2015, 1, 1)
+observedtime_end = date(2018, 1, 1)
+# ------------------------------
+
+
+# ---------- INITIAL -----------
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
+datetimeFormat = "%Y-%m-%d"
+plotdata = {}  # key = day in observedtime, value = value of selected mode
+# ------------------------------
+
+
+
+log_starttime = datetime.datetime.now()
+
+
+
+def daterange(observedtime_start, observedtime_end):
+    for n in range(int((observedtime_end - observedtime_start).days + 1)):
+        yield observedtime_start + timedelta(n)
+
+
+
+for threshold in thresholds:
+
+    plotdata[str(threshold)] = {}
+
+    for single_date in daterange(observedtime_start, observedtime_end):
+        plotdata[str(threshold)][str(single_date.strftime("%Y-%m-%d"))] = 0
+
+
+
+logging.info("Loading data ...")
+with open(path_source_streakdata, "r") as fp:
+    streakdata = json.load(fp)
+if path_user_restriction != "" or gender_restriction != "" or country_restriction != []:
+    with open(path_source_userdata, "r") as fp:
+        userdata = json.load(fp)
+if path_user_restriction != "":
+    with open(path_user_restriction, "r") as fp:
+        userids_restricted = json.load(fp)
+if gender_restriction != "" or country_restriction != []:
+    with open(path_source_genderdata, "r") as fp:
+        genderdata = json.load(fp)
+logging.info("Done (1/5)")
+
+
+
+logging.info("Comuting user set ...")
+
+if path_user_restriction != "" or gender_restriction != "":
+
+    delIDs = set()
+
+    if path_user_restriction != "":
+        for userid in streakdata:
+            if userid not in userids_restricted:
+                delIDs.add(userid)
+
+    if gender_restriction != "":
+        for userid in streakdata:
+            if userid not in genderdata:
+                delIDs.add(userid)
+            elif genderdata[userid]["gender"] != gender_restriction:
+                delIDs.add(userid)
+    
+    if country_restriction != []:
+        for userid in streakdata:
+            if userid not in genderdata:
+                delIDs.add(userid)
+            elif genderdata[userid]["country"] not in country_restriction:
+                delIDs.add(userid)
+
+    for userid in delIDs:
+        del streakdata[userid]
+
+logging.info("Done. (2/5)")
+
+
+
+logging.info("Computing usergroupsize data ...")
+
+if path_user_restriction == "" and gender_restriction == "":
+    with open(path_source_usergroupsize, "r") as fp:
+        usergroupsize = json.load(fp)
+else:
+    usergroupsize = {}
+
+    for day in daterange(observedtime_start, observedtime_end):
+        usergroupsize[str(day)] = 0
+
+    for userid in streakdata:
+        if userid in userdata:
+            created = datetime.datetime.strptime(str(userdata[userid]["created_at"]), "%Y-%m-%d %H:%M:%S").date()
+            if created >= observedtime_start and created <= observedtime_end:
+                for day in daterange(created, observedtime_end):
+                    usergroupsize[str(day)] += 1
+            elif created < observedtime_start:
+                for day in daterange(observedtime_start, observedtime_end):
+                    usergroupsize[str(day)] += 1
+
+logging.info("Done. (3/5)")
+
+
+
+logging.info("Starting ...")
+
+cnt_streaks = 0
+
+for userid in streakdata:
+    for streakid in streakdata[userid]:
+
+        start = datetime.datetime.strptime(str(streakdata[userid][streakid]["start"]), datetimeFormat).date()
+        end = datetime.datetime.strptime(str(streakdata[userid][streakid]["end"]), datetimeFormat).date()
+        length = int(streakdata[userid][streakid]["len"])
+
+        cnt_streaks += 1
+        if cnt_streaks % 1000000 == 0:
+            logging.info(str(cnt_streaks/1000000) + " million streaks computed.")
+
+        for threshold in thresholds:
+
+            if length >= threshold:
+                # streak happend (partially) in observed time
+                if start <= observedtime_end and end >= observedtime_start:
+
+                    a = max(start, observedtime_start)
+                    b = min(end, observedtime_end)
+
+                    for day in daterange(a, b):
+                        plotdata[str(threshold)][str(day)] += 1
+
+logging.info("Done. (4/5)")
+
+
+
+logging.info("Creating plot data ...")
+
+for threshold in thresholds:
+    for day in daterange(observedtime_start, observedtime_end):
+        plotdata[str(threshold)][str(day)] /= usergroupsize[str(day)]
+
+with open(path_results, "w") as fp:
+    json.dump(plotdata, fp)
+logging.info("Plot data saved.")
+
+logging.info("Done. (5/5)")
+
+
+log_endtime = datetime.datetime.now()
+log_runtime = (log_endtime - log_starttime)
+logging.info("Total runtime: " + str(log_runtime))
