2b0b6f3eecd1f85f3ee832990b6d544d11a655f6
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-03-05 12:12:14 +0100

    Update plotZScore.py

--- plot/streakDensity/plotZScore.py
+++ plot/streakDensity/plotZScore.py
@@ -46,7 +46,7 @@ max_before = 0
 cnt_before = 0
 for userid in plotdata_before:
     before.append(plotdata_before[userid])
-    if plotdata_before[userid] > 50:
+    if plotdata_before[userid] > 40:
         cnt_before += 1
     max_before = max(plotdata_before[userid], max_before)
 
@@ -54,25 +54,25 @@ max_after = 0
 cnt_after = 0
 for userid in plotdata_after:
     after.append(plotdata_after[userid])
-    if plotdata_after[userid] > 50:
+    if plotdata_after[userid] > 40:
         cnt_after += 1
     max_after = max(plotdata_after[userid], max_after)
 
 #sns.distplot(before, bins=10, kde=True, rug=True)
 #sns.distplot(after, bins=10, kde=True, rug=True)
 
-plt.hist(before, bins=500, density=True, range=(-10,50), color="#1F77B4", label="Before")
-plt.axvline(x=np.mean(before), color='k', label="Mean before = " + str(round(np.mean(before), 2)))
+plt.hist(after, histtype='step', bins=5000, density=True, range=(-10,50), color="#32A875", label="After", cumulative=True, linewidth=1.2)
+plt.axvline(x=np.mean(after), color='#32A875', label="Mean after = " + str(round(np.mean(after), 2)), linewidth=1.3)
 
-plt.hist(after, bins=500, density=True, range=(-10,50), color="#FF7F0E", label="After")
-plt.axvline(x=np.mean(after), color='k', label="Mean after = " + str(round(np.mean(after), 2)))
+plt.hist(before, histtype='step', bins=5000, density=True, range=(-10,50), color="#17719B", label="Before", cumulative=True, linewidth=1.2)
+plt.axvline(x=np.mean(before), color='#17719B', label="Mean before = " + str(round(np.mean(before), 2)), linewidth=1.3)
 
-plt.xlabel("z score distribution", fontsize=12)
-plt.ylabel("%", fontsize=12)
-plt.axvline(x=1.96, color='r', label="x = 1.96")
+plt.xlabel("z score distribution", fontsize=13)
+plt.ylabel("Share of users with z score ≤ x (CDF)", fontsize=13)
+plt.axvline(x=1.96, color='#D3685D', label="x = 1.96")
 
-plt.legend()
-#plt.show()
+plt.legend(loc='lower right', bbox_to_anchor=(1, 0.05), fontsize=11)
+plt.show()
 
 
 logging.info("Share BEFORE: " + str(100*cnt_before/len(before)))
