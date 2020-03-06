94d477e14f4e973b46393edf74b280073e997c30
lukasmoldon <49486044+lukasmoldon@users.noreply.github.com>
2020-03-06 23:13:33 +0100

    Update computeAssortativity.py

--- socialAnalysis/networks/computeAssortativity.py
+++ socialAnalysis/networks/computeAssortativity.py
@@ -4,6 +4,8 @@ import datetime
 import json
 import networkx as nx
 import random
+import numpy as np
+import math
 # ------------------------------
 
 
@@ -21,6 +23,7 @@ path_source_attributes ="/home/lmoldon/data/maximumStreak.json"
 # ---------- CONFIG ------------
 # corresponds to "t-1" in thesis (record > t-1 => streaker) 
 streakerThreshold = 7
+samplesize = 1000 # only for random model (avg)
 # ------------------------------
 
 
@@ -44,7 +47,6 @@ logging.info("Done (1/2)")
 logging.info("Starting ... ")
 
 G = nx.Graph() # real empirical graph
-R = nx.Graph() # same graph with attributes shuffled attribute values
 
 ids = list(data_attributes.keys())
 
@@ -54,22 +56,65 @@ for userid in data_attributes:
         G.add_node(userid, streaker="yes")
     else:
         G.add_node(userid, streaker="no")
-    if data_attributes[random.choice(ids)] > streakerThreshold:
-        R.add_node(userid, streaker="yes")
-    else:
-        R.add_node(userid, streaker="no")
 
 logging.info("Adding edges ... ")
 for firstnode in data_edges:
     for secondnode in data_edges[firstnode]:
         if firstnode in G.nodes() and secondnode in G.nodes():
             G.add_edge(firstnode, secondnode)
-            R.add_edge(firstnode, secondnode)
 
+logging.info("Computing random model ... ")
+coeff = []
+p = []
+i = 1
+while i <= samplesize:
+    R = nx.Graph() # same graph with attributes shuffled attribute values
+    for userid in data_attributes:
+        if data_attributes[random.choice(ids)] > streakerThreshold:
+            R.add_node(userid, streaker="yes")
+        else:
+            R.add_node(userid, streaker="no")
+    for firstnode in data_edges:
+        for secondnode in data_edges[firstnode]:
+            if firstnode in G.nodes() and secondnode in G.nodes():
+                R.add_edge(firstnode, secondnode)
+    coeff.append(nx.attribute_assortativity_coefficient(R,"streaker"))
+
+    streaker_attr = nx.get_node_attributes(R, "streaker")
+    cnt_pos = 0
+    cnt_neg = 0
+    for node in R.nodes():
+        if streaker_attr[node] == "yes":
+            found = False
+            for neighbor in R.neighbors(node):
+                if streaker_attr[neighbor] == "yes":
+                    found = True
+            if found:
+                cnt_pos += 1
+            else:
+                cnt_neg += 1
+    p.append(cnt_pos/(cnt_neg+cnt_pos))
+
+    if i%10==0: 
+        logging.info(str(i) + " random graphs computed")
+    i += 1
+
+avg = np.mean(coeff)
+real = nx.attribute_assortativity_coefficient(G,"streaker")
+
+stddeviation = 0
+for value in coeff:
+    stddeviation += ((value - avg) ** 2)
+stddeviation = math.sqrt(stddeviation / samplesize)
+
+zscore = 0
+if stddeviation != 0:
+    zscore = ((real - avg) / stddeviation)
 
 logging.info("Computing ... ")
-logging.info("Real assortativity coefficient: " +  str(nx.attribute_assortativity_coefficient(G,"streaker")))
-logging.info("Shuffled assortativity coefficient: " +  str(nx.attribute_assortativity_coefficient(R,"streaker")))
+logging.info("Real assortativity coefficient: " +  str(real))
+logging.info("Shuffled assortativity coefficient (avg): " +  str(avg))
+logging.info("zScore assortativity: " +  str(zscore))
 
 
 streaker_attr = nx.get_node_attributes(G, "streaker")
@@ -85,22 +130,9 @@ for node in G.nodes():
             cnt_pos += 1
         else:
             cnt_neg += 1
-logging.info("Real: P(n has streaking neighbor | n is streaker) = " + str(cnt_pos/(cnt_neg+cnt_pos)))
 
-streaker_attr = nx.get_node_attributes(R, "streaker")
-cnt_pos = 0
-cnt_neg = 0
-for node in R.nodes():
-    if streaker_attr[node] == "yes":
-        found = False
-        for neighbor in R.neighbors(node):
-            if streaker_attr[neighbor] == "yes":
-                found = True
-        if found:
-            cnt_pos += 1
-        else:
-            cnt_neg += 1
-logging.info("Shuffled: P(n has streaking neighbor | n is streaker) = " + str(cnt_pos/(cnt_neg+cnt_pos)))
+logging.info("Real: P(n has streaking neighbor | n is streaker) = " + str(cnt_pos/(cnt_neg+cnt_pos)))
+logging.info("Shuffled (avg): P(n has streaking neighbor | n is streaker) = " + str(np.mean(p)))
 
 
 logging.info("Done (2/2)")
